5f2c6c730f79a30868b951f3e065b434b226d4e8
Sebastien Beau <sebastien.beau@akretion.com>
2013-09-19 20:32:33 -0700

    [REF] refactor split module sale_payment method into sale_payment_method and payment_method in order to be able to use the payment_method on the purchase order

--- sale_payment_method/__openerp__.py
+++ sale_payment_method/__openerp__.py
@@ -1,27 +1,28 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
-#                                                                             #
-#   sale_payment_method for OpenERP                                            #
-#   Copyright (C) 2011 Akretion Sébastien BEAU <sebastien.beau@akretion.com>  #
-#                                                                             #
-#   This program is free software: you can redistribute it and/or modify      #
-#   it under the terms of the GNU Affero General Public License as            #
-#   published by the Free Software Foundation, either version 3 of the        #
-#   License, or (at your option) any later version.                           #
-#                                                                             #
-#   This program is distributed in the hope that it will be useful,           #
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of            #
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
-#   GNU Affero General Public License for more details.                       #
-#                                                                             #
-#   You should have received a copy of the GNU Affero General Public License  #
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.     #
-#                                                                             #
+#
+#   Module for OpenERP 
+#   Copyright (C) 2013 Akretion (http://www.akretion.com).
+#   @author Sébastien BEAU <sebastien.beau@akretion.com>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 ###############################################################################
 
 {
     'name': 'Sale Payment Method',
-    'version': '0.2',
+    'version': '0.3',
     'category': 'Generic Modules/Others',
     'license': 'AGPL-3',
     'description': """
@@ -38,12 +39,14 @@ payments entries on sales orders.
 """,
     'author': 'Akretion',
     'website': 'http://www.akretion.com/',
-    'depends': ['sale',
-                ],
-    'data': ['sale_view.xml',
-             'payment_method_view.xml',
-             'security/ir.model.access.csv',
-             ],
+    'depends': [
+        'sale',
+        'payment_method',
+    ],
+    'data': [
+        'sale_view.xml',
+        'payment_method_view.xml',
+    ],
     'demo': [],
     'installable': True,
 }
